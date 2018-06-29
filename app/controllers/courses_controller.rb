afe78b98f741c1dda9c34bb66034552443f829fe
0rr <sethwbrown22@gmail.com>
2018-06-29 02:37:12 +0000

    began individual course show page

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,10 +1,11 @@
-class CoursesController < ApplicationController
+class CoursesController < ApplicationController 
 
   def index
     @courses = Course.all
   end
 
   def show
+    @course = Course.find(params[:id])
   end
 
 end
